24512ad934f795c5d5e5ca39ee4aea78ba92368c
Ariffaja <akhi.arifin19@gmail.com>
2026-05-18 15:36:14 +0700

    .

---
+++ test_ml.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+print("Machine Learning Environment Ready")
